7969810816d94d41bce93e043fe8dfdc7865df6d
supreme429 <supreme0429@163.com>
2020-03-26 15:30:43 +0800

    fix: npm => yarn

--- src/project.js
+++ src/project.js
@@ -147,9 +147,9 @@ Project.prototype.generate = function() {
 
         // 安装依赖
         console.log();
-        const installSpinner = ora(`安装项目依赖 ${chalk.green.bold('npm install')}, 请稍后...`);
+        const installSpinner = ora(`安装项目依赖 ${chalk.green.bold('yarn install')}, 请稍后...`);
         installSpinner.start();
-        exec('npm install', (error, stdout, stderr) => {
+        exec('yarn install', (error, stdout, stderr) => {
           if (error) {
             installSpinner.color = 'red';
             installSpinner.fail(chalk.red('安装项目依赖失败，请自行重新安装！'));
